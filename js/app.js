5ee22a124b6180f94dbc59d4da89cd7c725aa03b
Maikon Marquez <37089888+MaikonJm@users.noreply.github.com>
2024-07-25 14:08:29 -0300

    Adiciona uma confirmação antes de devolver o jogo

--- js/app.js
+++ js/app.js
@@ -4,9 +4,12 @@ function alterarStatus(id) {
     let botao = gameClicado.querySelector('.dashboard__item__button');
     
     if (imagem.classList.contains('dashboard__item__img--rented')) {
-        imagem.classList.remove('dashboard__item__img--rented');
-        botao.classList.remove('dashboard__item__button--return');
-        botao.textContent = 'Alugar';
+        // Adiciona uma confirmação antes de devolver o jogo
+        if (confirm(`Você tem certeza que deseja devolver o jogo ${nomeJogo.textContent}?`)) {
+            imagem.classList.remove('dashboard__item__img--rented');
+            botao.classList.remove('dashboard__item__button--return');
+            botao.textContent = 'Alugar';
+        }
     } else {
         imagem.classList.add('dashboard__item__img--rented');
         botao.classList.add('dashboard__item__button--return');
